b37740b78c3a2c28fb2d3b4b30c86b8ee801d63a
hwa.j <hwajin0720@naver.com>
2025-01-14 17:59:32 +0900

    Fix: E2E 통합테스트 제거

--- src/test/java/kr/hhplus/be/server/integration_test/application/set_up/ReservationSetUp.java
+++ src/test/java/kr/hhplus/be/server/integration_test/application/set_up/ReservationSetUp.java
@@ -1,4 +1,4 @@
-package kr.hhplus.be.server.integration_test.inter.set_up;
+package kr.hhplus.be.server.integration_test.application.set_up;
 
 import kr.hhplus.be.server.domain.reservation.Reservation;
 import kr.hhplus.be.server.domain.reservation.ReservationState;
